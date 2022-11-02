f6643cacc380cf211e342e9290a3cbdc2c78b19f
junghyeon1.jung <junghyeon1.jung@lge.com>
2022-11-02 14:00:56 +0900

    백준 다익스트라 문제 풀이 추가

--- bj/dfs,bfs/1261.py
+++ bj/dfs,bfs/1261.py
@@ -1,6 +1,39 @@
 import sys
-import heapq
+from collections import deque
 
 
 def Input_Data():
     readl = sys.stdin.readline
+    N, M = map(int, readl().split())
+    maps = [list(map(int, readl().rstrip())) for _ in range(M)]
+    return N, M, maps
+
+
+N, M, maps = Input_Data()
+
+
+def Solve():
+    q = deque()
+    dir = ((-1, 0), (1, 0), (0, -1), (0, 1))
+    q.append((0, 0))
+    dist = [[-1] * N for _ in range(M)]
+    dist[0][0] = 0
+    while q:
+        curRow, curCol = q.popleft()
+        for r_d, c_d in dir:
+            nextRow, nextCol = curRow+r_d, curCol+c_d
+            if nextRow < 0 or nextCol < 0:
+                continue
+            if nextRow >= M or nextCol >= N:
+                continue
+            if dist[nextRow][nextCol] == -1:
+                if maps[nextRow][nextCol] == 1:
+                    dist[nextRow][nextCol] = dist[curRow][curCol]+1
+                    q.append((nextRow, nextCol))
+                if maps[nextRow][nextCol] == 0:
+                    dist[nextRow][nextCol] = dist[curRow][curCol]
+                    q.appendleft((nextRow, nextCol))
+    print(dist[M-1][N-1])
+
+
+Solve()
