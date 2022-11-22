5ec20d4d246c81eb050f4b7658b5eb60a9cfe0ac
luckjjh <luckjjh1@naver.com>
2022-11-22 16:53:21 +0900

    7일차 추가 문제 풀이

---
+++ sw-edu/day7/cunning_bisect.py
@@ -0,0 +1,25 @@
+import sys
+import bisect
+
+def Input_Data():
+	readl = sys.stdin.readline
+	N = int(readl())
+	list_file = list(map(int, readl().split()))
+	return N, list_file
+
+
+sol = 0
+
+
+N, list_file = Input_Data()
+list_file.sort()
+
+def Solve():
+  global sol
+  for i in range(len(list_file)-1):
+    curScore = list_file[i]
+    curMax = curScore*1.1
+    right = bisect.bisect_right(list_file,curMax)
+    sol+=(right-(i+1))
+  print(sol)
+Solve()
---
+++ sw-edu/day7/lisaBirthday.py
@@ -0,0 +1,49 @@
+import sys
+import math
+def Input_Data():
+  readl = sys.stdin.readline
+  N,M = map(int,readl().split())
+  # 재료 개수, 리사 돈
+  ingreList = [list(map(int,readl().split())) for _ in range(N)]
+  # 완성 위해 필요 양, 현재 주방에 있는양
+  # 작은 포장에 담긴 재료 양, 재료 가격
+  # 큰포장에 담겨있는 재료 양, 재료 가격
+  return N,M,ingreList
+
+N,M,ingreList = Input_Data()
+
+def Solve():
+  global sol
+  left = 0
+  right = 100000
+  while left<=right:
+    mid = (left+right)//2
+    curMoney = M
+    check = True
+    for i in range(len(ingreList)):
+      toMake,remain,small,smallCost,big,bigCost = ingreList[i]
+      curNeed = mid * toMake - remain 
+      # mid개 만큼 만드는 데 필요한 양에서 주방에 있는 양 빼서 현재 필요한 양 계산
+      curMin = sys.maxsize
+      for j in range(curNeed//small):
+        k = math.ceil((curNeed - small * j)/big)
+        # 재료가 남게 사야되므로 올림 연산
+        if k<0:
+          k = 0
+        # big 살때 k 0인 경우 고려
+        if j*smallCost+k*bigCost<curMin:
+          curMin = j*smallCost+k*bigCost
+      curMoney -= curMin
+      if curMoney<0:
+        check = False
+        break
+    if check:
+      left = mid+1
+    else:
+      right = mid -1
+  print(right)
+
+
+      
+
+Solve()
